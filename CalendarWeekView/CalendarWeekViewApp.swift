4965e156104edfef3417a3ad27f1cd822359969a
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2025-05-05 10:57:00 +0530

    Initial Commit

---
+++ CalendarWeekView/CalendarWeekViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarWeekViewApp.swift
+//  CalendarWeekView
+//
+//  Created by Rahul Ravi Prakash on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarWeekViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
